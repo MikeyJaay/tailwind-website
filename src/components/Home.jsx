0217ba73e8c0a2df2c7d06262f3a33d301bcaebf
Michael  Jaroszynski <mikeyj@Mikey-Js-MacBook-Pro.local>
2024-03-05 20:42:57 -0500

    pushing changes for night

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,28 @@ import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
-  return <div className="w-full h-screen bg-[#0a192f]">
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auot px-8 flex flex-col justify-center h-full">
+        <p>Hi, my name is</p>
+        <h1>Michael Jaroszynskit</h1>
+        <h2>I'm a Full Stack Developer</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, non
+          asperiores deserunt, aliquam officia exercitationem in facilis a
+          commodi ea error quis cum deleniti repellat quaerat repellendus
+          reprehenderit ullam magni? </p>
+          <div>
+            <button>View Work <HiArrowNarrowRight/> </button>
+          </div>
 
-  </div>;
+
+
+
+
+      </div>
+    </div>
+  );
 };
 
 export default Home;
